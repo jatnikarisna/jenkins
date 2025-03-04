909ad5917879cb2678e4ea09e2d649caf251974d
jatnikarisna <jatnikarisna@gmail.com>
2025-03-04 21:55:06 +0700

    954PM

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World - Jenkins - CI/CD - 04th march 2025');
+  res.send('Hello World - Jenkins - CI/CD - 04th march 2025 9:54PM');
 });
 
 app.listen(PORT, HOST, () => {
